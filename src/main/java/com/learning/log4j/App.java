c370ae4eb4f05f3639372ba370c05fc11453809e
Navin_K <navin.kare@gmail.com>
2019-03-07 12:47:20 -0600

    updated files

--- src/main/java/com/learning/log4j/App.java
+++ src/main/java/com/learning/log4j/App.java
@@ -1,13 +1,23 @@
 package com.learning.log4j;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger LOG = LogManager.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LOG.debug("This Will Be Printed On Debug");
+        LOG.info("This Will Be Printed On Info");
+        LOG.warn("This Will Be Printed On Warn");
+        LOG.error("This Will Be Printed On Error");
+        LOG.fatal("This Will Be Printed On Fatal");
+
+        LOG.info("Appending string: {}.", "Hello, World");
     }
 }
